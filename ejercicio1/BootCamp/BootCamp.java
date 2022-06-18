6919c9eb3044e38a070ed62d528968910053663e
carlos8788 <100473510+carlos8788@users.noreply.github.com>
2022-06-18 16:44:12 -0300

    Add files via upload

---
+++ ejercicio1/BootCamp/BootCamp.java
@@ -0,0 +1,34 @@
+package ejercicio1.BootCamp;
+
+public class BootCamp {
+    public static void main(String[] args) {
+        //ENTEROS
+        byte numA = 10;
+        short numB = 250;
+        int numC = 400000;
+        long numD = 999999999;
+
+        //DECIMALES
+        float decA = 13.2f;
+        double decB = 23.67;
+
+        //CARACTER
+        char charA = 'a';
+
+        //CADENA
+        String cadena = "Hola Mundo";
+
+        //BOOLEANO
+        boolean verdadero = true;
+        boolean falso = false;
+
+        System.out.println("numeros enteros: " + numA + " ; " + numB + " ; " + numC + " ; " + numD);
+
+        System.out.println("Numeros Decimales: "+ decA + " ; " + decB);
+
+        System.out.println("Carcater y cadena: " + charA + " ; " + cadena);
+
+        System.out.println("Booleanos: " + verdadero + " ; " + falso);
+    }
+
+}
